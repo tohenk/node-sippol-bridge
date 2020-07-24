a2afe92a1cfd3781dee9e80673e45e71e464671d
Toha <tohenk@yahoo.com>
2020-07-24 10:39:12 +0700

    Refactor app into class.
    Signed-off-by: Toha <tohenk@yahoo.com>

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const fs = require('fs');
 const path = require('path');
 const Cmd = require('./lib/cmd');
 const Work = require('./lib/work');
+const { SippolBridge, SippolQueue } = require('./bridge');
 
 Cmd.addBool('help', 'h', 'Show program usage').setAccessible(false);
 Cmd.addVar('config', 'c', 'Set configuration file', 'filename');
@@ -39,174 +40,211 @@ if (!Cmd.parse() || (Cmd.get('help') && usage())) {
     process.exit();
 }
 
-(function run() {
-    let config = {}, profile, filename;
-    // read configuration from command line values
-    filename = Cmd.get('config') ? Cmd.get('config') : path.join(__dirname, 'config.json');
-    if (fs.existsSync(filename)) {
-        console.log('Reading configuration %s', filename);
-        config = JSON.parse(fs.readFileSync(filename));
-    }
-    if (Cmd.get('url')) config.url = Cmd.get('url');
-    if (Cmd.get('username')) config.username = Cmd.get('username');
-    if (Cmd.get('password')) config.password = Cmd.get('password');
-    if (!config.workdir) config.workdir = __dirname;
+class App {
 
-    if (!config.username || !config.password) {
-        console.error('Both username or password must be supplied!');
-        return;
+    config = {}
+    uploads = {}
+    bridge = null
+    bridge2 = null
+
+    initialize() {
+        let filename, profile;
+        // read configuration from command line values
+        filename = Cmd.get('config') ? Cmd.get('config') : path.join(__dirname, 'config.json');
+        if (fs.existsSync(filename)) {
+            console.log('Reading configuration %s', filename);
+            this.config = JSON.parse(fs.readFileSync(filename));
+        }
+        if (Cmd.get('url')) this.config.url = Cmd.get('url');
+        if (Cmd.get('username')) this.config.username = Cmd.get('username');
+        if (Cmd.get('password')) this.config.password = Cmd.get('password');
+        if (!this.config.workdir) this.config.workdir = __dirname;
+
+        if (!this.config.username || !this.config.password) {
+            console.error('Both username or password must be supplied!');
+            return;
+        }
+        // load form maps
+        filename = path.join(__dirname, 'maps.json');
+        if (fs.existsSync(filename)) {
+            this.config.maps = JSON.parse(fs.readFileSync(filename));
+            console.log('Maps loaded from %s', filename);
+        }
+        // load doc maps
+        filename = path.join(__dirname, 'docs.json');
+        if (fs.existsSync(filename)) {
+            this.config.docs = JSON.parse(fs.readFileSync(filename));
+            console.log('Document maps loaded from %s', filename);
+        }
+        // load profile
+        this.config.profiles = {};
+        filename = path.join(__dirname, 'profiles.json');
+        if (fs.existsSync(filename)) {
+            const profiles = JSON.parse(fs.readFileSync(filename));
+            if (profiles.profiles) this.config.profiles = profiles.profiles;
+            if (profiles.active) profile = profiles.active;
+        }
+        if (Cmd.get('profile')) profile = Cmd.get('profile');
+        if (profile && this.config.profiles[profile]) {
+            console.log('Using profile %s', profile);
+            const keys = ['timeout', 'wait', 'delay', 'opdelay'];
+            for (let key in this.config.profiles[profile]) {
+                if (keys.indexOf(key) < 0) continue;
+                this.config[key] = this.config.profiles[profile][key];
+            }
+        }
+        return true;
     }
-    // load form maps
-    filename = path.join(__dirname, 'maps.json');
-    if (fs.existsSync(filename)) {
-        config.maps = JSON.parse(fs.readFileSync(filename));
-        console.log('Maps loaded from %s', filename);
-    }
-    // load doc maps
-    filename = path.join(__dirname, 'docs.json');
-    if (fs.existsSync(filename)) {
-        config.docs = JSON.parse(fs.readFileSync(filename));
-        console.log('Document maps loaded from %s', filename);
-    }
-    // load profile
-    config.profiles = {};
-    filename = path.join(__dirname, 'profiles.json');
-    if (fs.existsSync(filename)) {
-        const profiles = JSON.parse(fs.readFileSync(filename));
-        if (profiles.profiles) config.profiles = profiles.profiles;
-        if (profiles.active) profile = profiles.active;
-    }
-    if (Cmd.get('profile')) profile = Cmd.get('profile');
-    if (profile && config.profiles[profile]) {
-        console.log('Using profile %s', profile);
-        const keys = ['timeout', 'wait', 'delay', 'opdelay'];
-        for (let key in config.profiles[profile]) {
-            if (keys.indexOf(key) < 0) continue;
-            config[key] = config.profiles[profile][key];
+
+    createBridge() {
+        if (null == this.bridge) {
+            this.bridge = new SippolBridge(this.config);
         }
     }
 
-    const uploads = {};
-    const port = Cmd.get('port') | 3000;
-    const {SippolBridge, SippolQueue} = require('./bridge');
-    const bridge = new SippolBridge(config);
-    const http = require('http').createServer();
-    const io = require('socket.io')(http);
-    let bridge2 = null;
-    io.of('/sippol').on('connection', (socket) => {
+    createBridge2() {
+        if (null == this.bridge2) {
+            const config = Object.assign({}, this.config);
+            config.browser = 'firefox';
+            if (config.browser == this.config.browser) config.session = 's2';
+            this.bridge2 = new SippolBridge(config);
+        }
+    }
+
+    createServer() {
+        const port = Cmd.get('port') | 3000;
+        const http = require('http').createServer();
+        const io = require('socket.io')(http);
+        io.of('/sippol')
+            .on('connection', (socket) => {
+                this.handleConnection(socket);
+            })
+        ;
+        http.listen(port, () => {
+            console.log('Application ready on port %s...', port);
+            Work.works([
+                () => this.bridge.isReady(),
+                () => this.bridge.selfTest(),
+            ]);
+        });
+    }
+
+    handleConnection(socket) {
         console.log('Client connected: %s', socket.id);
-        socket.on('disconnect', () => {
-            console.log('Client disconnected: %s', socket.id);
-        });
-        socket.on('setup', (data) => {
-            if (data.callback) {
-                socket.callback = data.callback;
-            }
-            socket.emit('setup', {version: bridge.VERSION});
-        });
-        socket.on('spp', (data) => {
-            const queue = SippolQueue.createSppQueue(data, socket.callback);
-            const res = bridge.addQueue(queue);
-            queue.info = queue.getMappedData('penerima.penerima');
-            console.log('SPP: %s %s', data[bridge.datakey], queue.info ? queue.info : '');
-            socket.emit('spp', res);
-        });
-        socket.on('upload', (data) => {
-            let res;
-            if (data.Id) {
-                const queue = SippolQueue.createUploadQueue(data, socket.callback);
-                res = bridge.addQueue(queue);
-                if (data.info) queue.info = data.info;
-                console.log('Upload: %s %s', data.Id, queue.info ? queue.info : '');
-            } else {
-                const msg = 'Ignoring upload without Id';
-                console.log(msg);
-                res = {error: msg}
-            }
-            socket.emit('upload', res);
-        });
-        socket.on('upload-part', (data) => {
-            let res;
-            if (data.Id) {
-                if (data.seq == 1) {
-                    uploads[data.Id] = {Id: data.Id};
-                    if (data.info) uploads[data.Id].info = data.info;
-                    if (data.term) uploads[data.Id].term = data.term;
+        socket
+            .on('disconnect', () => {
+                console.log('Client disconnected: %s', socket.id);
+            })
+            .on('setup', (data) => {
+                if (data.callback) {
+                    socket.callback = data.callback;
                 }
-                let parts = [];
-                Object.keys(data).forEach((k) => {
-                    if (['Id', 'info', 'term', 'seq', 'tot'].indexOf(k) < 0) {
-                        uploads[data.Id][k] = data[k];
-                        parts.push(k);
+                socket.emit('setup', {version: this.bridge.VERSION});
+            })
+            .on('spp', (data) => {
+                const queue = SippolQueue.createSppQueue(data, socket.callback);
+                const res = this.bridge.addQueue(queue);
+                queue.info = queue.getMappedData('penerima.penerima');
+                console.log('SPP: %s %s', data[this.bridge.datakey], queue.info ? queue.info : '');
+                socket.emit('spp', res);
+            })
+            .on('upload', (data) => {
+                let res;
+                if (data.Id) {
+                    const queue = SippolQueue.createUploadQueue(data, socket.callback);
+                    res = this.bridge.addQueue(queue);
+                    if (data.info) queue.info = data.info;
+                    console.log('Upload: %s %s', data.Id, queue.info ? queue.info : '');
+                } else {
+                    const msg = 'Ignoring upload without Id';
+                    console.log(msg);
+                    res = {error: msg}
+                }
+                socket.emit('upload', res);
+            })
+            .on('upload-part', (data) => {
+                let res;
+                if (data.Id) {
+                    if (data.seq == 1) {
+                        this.uploads[data.Id] = {Id: data.Id};
+                        if (data.info) this.uploads[data.Id].info = data.info;
+                        if (data.term) this.uploads[data.Id].term = data.term;
                     }
-                });
-                if (parts.length) {
-                    if (data.seq == data.tot) {
-                        const udata = uploads[data.Id];
-                        const queue = SippolQueue.createUploadQueue(udata, socket.callback);
-                        res = bridge.addQueue(queue);
-                        if (udata.info) queue.info = udata.info;
-                        console.log('Upload from partial: %s %s', udata.Id, queue.info ? queue.info : '');
-                        delete uploads[data.Id];
+                    let parts = [];
+                    Object.keys(data).forEach((k) => {
+                        if (['Id', 'info', 'term', 'seq', 'tot'].indexOf(k) < 0) {
+                            this.uploads[data.Id][k] = data[k];
+                            parts.push(k);
+                        }
+                    });
+                    if (parts.length) {
+                        if (data.seq == data.tot) {
+                            const udata = this.uploads[data.Id];
+                            const queue = SippolQueue.createUploadQueue(udata, socket.callback);
+                            res = this.bridge.addQueue(queue);
+                            if (udata.info) queue.info = udata.info;
+                            console.log('Upload from partial: %s %s', udata.Id, queue.info ? queue.info : '');
+                            delete this.uploads[data.Id];
+                        } else {
+                            res = {part: parts};
+                        }
                     } else {
-                        res = {part: parts};
+                        res = {error: 'Document part not found for ' + data.Id};
                     }
                 } else {
-                    res = {error: 'Document part not found for ' + data.Id};
+                    const msg = 'Ignoring partial upload without Id';
+                    console.log(msg);
+                    res = {error: msg}
                 }
-            } else {
-                const msg = 'Ignoring partial upload without Id';
-                console.log(msg);
-                res = {error: msg}
-            }
-            socket.emit('upload-part', res);
-        });
-        socket.on('query', (data) => {
-            console.log('Query: %s', data.term);
-            const f = () => new Promise((resolve, reject) => {
-                const queue = SippolQueue.createQueryQueue({term: data.term}, socket.callback);
-                bridge.addQueue(queue);
-                queue.info = data.term;
-                queue.resolve = resolve;
-                queue.reject = reject;
-            });
-            f()
-                .then((items) => {
-                    socket.emit('query', {result: items});
-                })
-                .catch((err) => {
-                    socket.emit('query', {error: err instanceof Error ? err.message : err});
-                })
-            ;
-        });
-        socket.on('list', (data) => {
-            if (bridge2 == null) {
-                const cfg = Object.assign({}, config);
-                cfg.browser = 'firefox';
-                if (cfg.browser == config.browser) cfg.session = 's2';
-                bridge2 = new SippolBridge(cfg);
-            }
-            const options = {year: data.year};
-            ['spp', 'spm', 'sp2d'].forEach((key) => {
-                if (data[key] && (
-                    !isNaN(data[key]) || (typeof data[key] == 'string' && data[key].indexOf('T') > 0)
-                    )
-                ) {
-                    options[key] = new Date(data[key]);
-                }
-            });
-            const queue = SippolQueue.createListQueue(options, socket.callback);
-            const res = bridge2.addQueue(queue);
-            socket.emit('list', res);
-        });
-    });
-    http.listen(port, () => {
-        console.log('Application ready on port %s...', port);
-        Work.works([
-            () => bridge.isReady(),
-            () => bridge.selfTest(),
-        ]);
-    });
+                socket.emit('upload-part', res);
+            })
+            .on('query', (data) => {
+                console.log('Query: %s', data.term);
+                const f = () => new Promise((resolve, reject) => {
+                    const queue = SippolQueue.createQueryQueue({term: data.term}, socket.callback);
+                    this.bridge.addQueue(queue);
+                    queue.info = data.term;
+                    queue.resolve = resolve;
+                    queue.reject = reject;
+                });
+                f()
+                    .then((items) => {
+                        socket.emit('query', {result: items});
+                    })
+                    .catch((err) => {
+                        socket.emit('query', {error: err instanceof Error ? err.message : err});
+                    })
+                ;
+            })
+            .on('list', (data) => {
+                this.createBridge2();
+                const options = {year: data.year};
+                ['spp', 'spm', 'sp2d'].forEach((key) => {
+                    if (data[key] && (
+                        !isNaN(data[key]) || (typeof data[key] == 'string' && data[key].indexOf('T') > 0)
+                        )
+                    ) {
+                        options[key] = new Date(data[key]);
+                    }
+                });
+                const queue = SippolQueue.createListQueue(options, socket.callback);
+                const res = this.bridge2.addQueue(queue);
+                socket.emit('list', res);
+            })
+        ;
+    }
+
+    run() {
+        if (this.initialize()) {
+            this.createBridge();
+            this.createServer();
+            return true;
+        }
+    }
+}
+
+(function run() {
+    new App().run();
 })();
 
 function usage() {
